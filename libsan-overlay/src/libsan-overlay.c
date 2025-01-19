595e442a0b4dccaa78b107a34016ad64350b656d
karolKiedrowski <werberin20@gmail.com>
2025-01-20 00:38:59 +0100

    added writing sanitizing message to a file

--- libsan-overlay/src/libsan-overlay.c
+++ libsan-overlay/src/libsan-overlay.c
@@ -7,6 +7,18 @@
 /* Original definition: https://github.com/llvm/llvm-project/blob/4dc34b0d660a52744164a37466ce245764126296/compiler-rt/lib/sanitizer_common/sanitizer_printf.cpp#L302 */
 void __sanitizer_on_print(const char *str)
 {
-	printf("!!! This message has been intercepted by a lazy programmer (%zu) !!!\n", strlen(str));
+	FILE* pFile;
+	pFile = fopen("sanitizer-messages.txt", "a+");
+	if(pFile == NULL)
+	{
+		printf("File is not opened.\n");
+		return;
+	}
+
+	fprintf(pFile, "%s\n", str);
+
+	fclose(pFile);
+	
+	// printf("!!! This message has been intercepted by a lazy programmer (%zu) !!!\n", strlen(str));
 	/* TODO: write an interpreter of ASAN messages and emit MAGIC */
 }
